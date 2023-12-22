ca07fbe9f297220919a257e2ad1599e36a5c3fa3
AnnapurnaBantu <annapurnakommula.java@gmail.com>
2023-12-22 20:23:42 +0530

    Sample_Project

---
+++ SampleProgram/src/com/java/sample/SampleClass.java
@@ -0,0 +1,10 @@
+package com.java.sample;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Annapurna Education Repository");
+	}
+
+}
