50da4125d72a4d723c258e764138a6c59f4135ea
AnnapurnaBantu <annapurnakommula.java@gmail.com>
2023-12-22 20:50:54 +0530

    SecondCommit

---
+++ SampleProgram/src/com/java/sample/Program2.java
@@ -0,0 +1,11 @@
+package com.java.sample;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("#############################################");
+	}
+
+}
